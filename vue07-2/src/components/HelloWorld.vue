26d6ecde1ef77530114c69fb2640aa1582688d16
Ezequiel Bertti <ebertti@gmail.com>
2021-08-10 21:50:13 -0300

    vuetify

---
+++ vue07-2/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-form>
+        <v-input>
+          <v-text-field
+              v-model="teste"
+              :counter="20"
+              label="Exemplo"
+              required
+          ></v-text-field>
+        </v-input>
+
+          <v-text-field
+              v-model="teste"
+              :counter="20"
+              label="Exemplo"
+              required
+          ></v-text-field>
+
+
+      </v-form>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    teste: 'ezequiel'
+  }),
+}
+</script>
---
+++ vue07/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-input v-model="teste">
+
+      </v-input>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import logo from '../assets/logo.svg'
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    teste: 'ezequiel'
+  }),
+}
+</script>
