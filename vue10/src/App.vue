9b23dca72e5e1f77675b2266a05cd33d5bd734f9
Ezequiel Bertti <ebertti@gmail.com>
2021-08-19 22:08:50 -0300

    vue10

--- vue10/src/App.vue
+++ vue10/src/App.vue
@@ -3,27 +3,33 @@
   <div>
     <table>
       <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Estado</th>
-          <th>Torcida</th>
-          <th>Ano</th>
-          <th>info</th>
-        </tr>
+      <tr>
+        <th>Nome</th>
+        <th>Estado</th>
+        <th>Torcida</th>
+        <th>Ano</th>
+        <th>info</th>
+      </tr>
       </thead>
       <tbody>
-      <tr v-for="time in times">
+      <tr v-for="(time, index) in times">
         <td>{{ time.nome }}</td>
         <td>{{ time.estado }}</td>
         <td>{{ time.torcida }}</td>
         <td>{{ time.fundacao_ano }}</td>
         <td>{{ time.info }}</td>
+        <td>
+          <button @click="editar(time)">editar</button>
+        </td>
+        <td>
+          <span v-if="carregando">carregando</span>
+          <button v-else @click="apagar(time, index)">apagar</button>
+        </td>
       </tr>
       </tbody>
     </table>
   </div>
 
-  <TabelaMarota :lista="times"></TabelaMarota>
 
   <div>
 
@@ -58,6 +64,7 @@ import Campo from './components/Campo.vue'
 import axios from 'axios'
 import CampoDropDown from './components/CampoDropDown.vue'
 import CampoText from './components/CampoText.vue'
+import TabelaMarota from './components/TabelaMarota.vue'
 
 let timeNovo = () => {
   return {
@@ -71,22 +78,14 @@ let timeNovo = () => {
   }
 }
 
-let alunoNovo = () => {
-  return {
-    'id': 'INCREMENT',
-    'nome': '',
-    'serie': '',
-    'idade': '',
-  }
-}
-
 export default {
-  components: {CampoText, CampoDropDown, Campo},
+  components: {TabelaMarota, CampoText, CampoDropDown, Campo},
   data() {
     return {
       time: timeNovo(),
       times: [],
       carregando: true,
+      editando: false,
       estados: [
         'RJ',
         'SP',
@@ -97,14 +96,37 @@ export default {
   methods: {
     salvar() {
       this.carregando = true
-      axios.post(
-          'https://sheetdb.io/api/v1/87jz1jtmjtrf6',
-          {data: [this.time]}
-      ).then(() => {
-        this.times.push(this.time)
-        this.time = timeNovo()
+      if (this.editando) {
+        axios.put(
+            `https://sheetdb.io/api/v1/87jz1jtmjtrf6/id/${this.time.id}`,
+            {data: [this.time]}
+        ).then(() => {
+          Object.assign(this.editando, this.time)
+          this.time = timeNovo()
+          this.carregando = false
+          this.editando = false
+        })
+      } else {
+        axios.post(
+            'https://sheetdb.io/api/v1/87jz1jtmjtrf6',
+            {data: [this.time]}
+        ).then(() => {
+          this.times.push(this.time)
+          this.time = timeNovo()
+          this.carregando = false
+        })
+      }
+    },
+    apagar(time, index) {
+      this.carregando = true
+      axios.delete(`https://sheetdb.io/api/v1/87jz1jtmjtrf6/id/${time.id}`).then(() => {
+        this.times.splice(index, 1)
         this.carregando = false
       })
+    },
+    editar(time) {
+      this.editando = time
+      this.time = {...time}
     }
   },
   mounted() {
---
+++ vue10/src/components/TabelaMarota.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <thead>
+    <tr >
+      <th v-for="(atributo,key) in lista[0]">
+        {{ key }}</th>
+
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in lista">
+      <td v-for="(atributo,key) in item">
+        {{ atributo }}
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TabelaMarota',
+  props:['lista']
+}
+</script>
